4141da7736acf8c0e5b32f7e7310665870f9e270
foolshi <adamssommerville@gmail.com>
2026-03-20 13:55:24 -0400

    Adding the timer function

--- apm.sh
+++ apm.sh
@@ -4,6 +4,11 @@ IP_ADDR="192.168.1.100"
 INTERFACE="ems160"
 DISK_DEV="sda"
 
+get_elapsed_seconds() {
+  current_time=$(date +%s)
+  echo $((current_time - START_TIME))
+}
+
 start_process() {
     echo "Starting APM process..."
     # Simulate starting the APM process
